2802e7891a2d595c2716c380f01ff53f7db615c2
david-ouatedem <davdev1400@gmail.com>
2024-10-27 04:35:23 +0100

    fixed average speed

--- back-app/server.py
+++ back-app/server.py
@@ -3,16 +3,15 @@ from typing import List
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
-
-
 # Store latest data to serve via GET request
 latest_traffic_data = []
 
 app = FastAPI()
 
+# Enable CORS for frontend development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:5174"],  # Adjust the allowed origins as needed
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],  # Adjust the allowed origins as needed
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,7 +26,7 @@ async def get_traffic_data():
 @app.post("/traffic-data")
 async def post_traffic_data(data: List[dict]):
     global latest_traffic_data
-    latest_traffic_data = data
+    latest_traffic_data = data  # Update the latest traffic data
     return {"status": "Data received"}
 
 # Start FastAPI server if this file is run directly
